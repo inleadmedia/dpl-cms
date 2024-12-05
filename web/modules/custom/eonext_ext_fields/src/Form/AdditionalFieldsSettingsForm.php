58a9f3a3f942ca07aedbf57f2a1f3576a68c567d
OIOIOOOI <anatolii.mn@gmail.com>
2024-12-05 16:38:01 +0200

    EONEXT-95 - Expose additional fields.

---
+++ web/modules/custom/eonext_ext_fields/src/Form/AdditionalFieldsSettingsForm.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\eonext_ext_fields\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Additional fields settings form class.
+ */
+class AdditionalFieldsSettingsForm extends ConfigFormBase {
+
+  public const FORM_ID = 'eonext_ext_fields.settings_form';
+
+  public const CONFIG_ID = 'eonext_ext_fields.settings';
+
+  public const FBI_FIELD_PATTERN = '[a-z]+(\.[a-z]+)*';
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return [
+      self::CONFIG_ID,
+    ];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getFormId(): string {
+    return self::FORM_ID;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['additional_fields'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Additional fields to expose inside Works object'),
+      '#description' => $this->t('One entry per line. <br />LABEL|DOT.SEPARATED_PATH, e.g.: TvTitle|titles.tvSeries.title'),
+      '#config_target' => self::CONFIG_ID . ':' . 'additional_fields',
+    ];
+
+    $form['cover_override'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('User covers from this path within Works object'),
+      '#description' => $this->t('Fetch alternative covers from FBI well field, .e.g: "manifestations.latest.cover.detail"'),
+      '#config_target' => self::CONFIG_ID . ':' . 'cover_override',
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    parent::validateForm($form, $form_state);
+
+    $additional_fields = $form_state->getValue('additional_fields');
+    $additional_fields_parsed = [];
+    foreach (array_filter(preg_split("/\t|\r|\n/", $additional_fields)) as $additional_field_entry) {
+      $additional_field_entry = trim($additional_field_entry);
+      if (!preg_match('/^[a-z0-9\s]+\|' . self::FBI_FIELD_PATTERN . '$/i', $additional_field_entry)) {
+        $form_state->setErrorByName('additional_fields', $this->t('Failed to validate one or more entries.'));
+        break;
+      }
+
+      $additional_fields_parsed[] = $additional_field_entry;
+    }
+
+    $form_state->setValue('additional_fields', implode("\n", $additional_fields_parsed));
+
+    $cover_override = $form_state->getValue('cover_override');
+    if (!preg_match('/^' . self::FBI_FIELD_PATTERN . '$/i', $cover_override)) {
+      $form_state->setErrorByName('cover_override', $this->t('Failed to validate pattern.'));
+    }
+  }
+
+}
